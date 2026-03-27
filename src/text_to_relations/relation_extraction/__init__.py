5d4a31d6f7dc415ab8fa8b4fb3361c2b34ddc457
jkurlandski <jkurlandski@gmail.com>
2026-03-27 17:08:33 -0400

    Re-export Annotation, TokenAnn, SentenceAnn, and ExtractionPhaseABC from init.py for shorter client imports.

--- src/text_to_relations/relation_extraction/__init__.py
+++ src/text_to_relations/relation_extraction/__init__.py
@@ -1,3 +1,7 @@
 from text_to_relations.relation_extraction.RegexString import RegexString
+from text_to_relations.relation_extraction.Annotation import Annotation
+from text_to_relations.relation_extraction.TokenAnn import TokenAnn
+from text_to_relations.relation_extraction.SentenceAnn import SentenceAnn
+from text_to_relations.relation_extraction.ExtractionPhaseABC import ExtractionPhaseABC
 
-__all__ = ["RegexString"]
+__all__ = ["RegexString", "Annotation", "TokenAnn", "SentenceAnn", "ExtractionPhaseABC"]
