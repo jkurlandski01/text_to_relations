71cb6c4e5d28da5a9773c6a7c1175acfa454adbb
jkurlandski <jkurlandski@gmail.com>
2026-04-02 21:44:41 -0400

    Miscellaneous minor changes.

--- src/text_to_relations/relation_extraction/__init__.py
+++ src/text_to_relations/relation_extraction/__init__.py
@@ -2,6 +2,6 @@ from text_to_relations.relation_extraction.RegexString import RegexString
 from text_to_relations.relation_extraction.Annotation import Annotation
 from text_to_relations.relation_extraction.TokenAnn import TokenAnn
 from text_to_relations.relation_extraction.SentenceAnn import SentenceAnn
-from text_to_relations.relation_extraction.ExtractionPhaseABC import ExtractionPhaseABC, SimpleExtractionPhase
+from text_to_relations.relation_extraction.ExtractionPhaseABC import ExtractionPhaseABC, SimpleExtractionPhase, ChainLink
 
-__all__ = ["RegexString", "Annotation", "TokenAnn", "SentenceAnn", "ExtractionPhaseABC", "SimpleExtractionPhase"]
+__all__ = ["RegexString", "Annotation", "TokenAnn", "SentenceAnn", "ExtractionPhaseABC", "SimpleExtractionPhase", "ChainLink"]
